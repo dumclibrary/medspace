83a821914bb7ba27029b1520be696c7dde8502b9
Russell Koonts <rskoonts@duke.edu>
2017-08-16 11:16:53 -0400

    objects need a physical location

--- app/models/image.rb
+++ app/models/image.rb
@@ -7,7 +7,13 @@ class Image < ActiveFedora::Base
   # self.valid_child_concerns = []
   validates :title, presence: { message: 'Your work must have a title.' }
 
+
     self.indexer = ImageIndexer
-    
+
   self.human_readable_type = 'Image'
+
+  property :holding_entity, predicate: ::RDF::Vocab::MODS.locationPhysical do |index|
+    index.as :stored_searchable, :facetable
+  end
+
 end
