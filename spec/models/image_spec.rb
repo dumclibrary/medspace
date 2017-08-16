510a3f55627cc6ecf0e57f655235b34ce120d498
Russell Koonts <rskoonts@duke.edu>
2017-08-16 10:22:39 -0400

    ensured that title is required

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe Image do
     i.save
     expect(i).to be_instance_of(Image)
   end
+
+  it "can add a title" do
+    i = Image.new
+    i.title = ["Test Title"]
+    i.save
+    expect(i.title.first).to eq "Test Title"
+    # puts i.resource.dump(:ttl)
+    # <http://purl.org/dc/terms/title> "Test Title";
+    expect(i.resource.dump(:ttl)).to match(/purl.org\/dc\/terms\/title/)
+  end
+
+  it "requires a title" do
+    image = Image.new
+    expect(image.save).to eq false
+    image.title = ["test"]
+    expect(image.save).to eq true
+  end
 end
