c88097fb44805ca155cbb14031273d13b338a707
Russell Koonts <rskoonts@duke.edu>
2017-08-21 14:53:58 -0400

    allow users to follow links to external objects

--- spec/features/show_external_object_feature.rb
+++ spec/features/show_external_object_feature.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
-RSpec.feature 'Display an Image' do
+RSpec.feature 'Display an External Object' do
   let(:title) { ['Apothecary jar. Label: Pom: Pice N:'] }
   let(:archival_collection) { ['Duke Medical Center Library & Archives'] }
+  let(:related_url) { ['http://archives.mc.duke.edu']}
   let(:date) { ['1973-04-09'] }
   let(:holding_entity) { ['Medicl Center Archives'] }
   let(:visibility) { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
   let :exo do
     ExternalObject.new(title: title,
               archival_collection: archival_collection,
+              related_url: related_url,
               holding_entity: holding_entity,
               date: date,
               visibility: visibility)
@@ -30,6 +32,7 @@ RSpec.feature 'Display an Image' do
       login_as user
       visit("/concern/external_objects/#{exo.id}")
       expect(page).to have_content exo.title.first
+      expect(page).to have_selector "a[href='#{related_url.first}']"
       expect(page).to have_content exo.archival_collection.first
       expect(page).to have_content exo.date.first
       expect(page).to have_content exo.holding_entity.first
@@ -38,6 +41,7 @@ RSpec.feature 'Display an Image' do
     scenario "Show an External Object unauthenticated user" do
       visit("/concern/external_objects/#{exo.id}")
       expect(page).to have_content exo.title.first
+      expect(page).to have_selector "a[href='#{related_url.first}']"
       expect(page).to have_content exo.archival_collection.first
       expect(page).not_to have_content exo.date.first
       expect(page).to have_content exo.holding_entity.first
