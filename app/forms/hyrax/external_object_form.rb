7b141358427a3f93feb882b5c080b03889a9eced
Russell Koonts <rskoonts@duke.edu>
2017-10-11 14:41:47 -0400

    changed required metadata to remove description on document and external objects

--- app/forms/hyrax/external_object_form.rb
+++ app/forms/hyrax/external_object_form.rb
@@ -4,7 +4,7 @@ module Hyrax
   class ExternalObjectForm < Hyrax::Forms::WorkForm
     self.model_class = ::ExternalObject
     self.terms += [:resource_type, :holding_entity, :date, :archival_collection]
-    self.required_fields +=[:description, :date_created, :subject]
+    self.required_fields +=[:date_created, :subject]
     self.required_fields -=[:creator, :rights_statement, :keyword]
     self.terms -= [:source, :rights_statement, :license, :based_near, :location, :language, :keyword]
   end
