f978dcc9f2d4002fc57a066d129bedd1c3aa151d
Russell Koonts <rskoonts@duke.edu>
2017-10-25 12:27:54 -0400

    removed blacklight-spotlight

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,9 @@ class User < ApplicationRecord
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  #devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
