2c1af5abc6988ce9cde6628f644b280ede655fce
Jennifer Lindner <jenlindner@gmail.com>
2018-02-09 15:05:41 -0500

    fix file-import test

--- spec/lib/medspace/importer/import_file_spec.rb
+++ spec/lib/medspace/importer/import_file_spec.rb
@@ -5,7 +5,8 @@ describe Medspace::ImportFile do
   let(:first_record) { doc.xpath("//object") }
   let(:record) { Medspace::Record.new(first_record) }
   let(:data_path) { Rails.root.join('spec', 'fixtures', 'files', 'importer', 'image') }
-  let(:file_path) { File.join(data_path, 'test.pdf') }
+  let(:file) { 'test.pdf' }
+  let(:full_path) { File.join(data_path, 'test.pdf') }
   let(:bad_file_path) { File.join(data_path, 'bad_file_path.pdf') }
   let(:user) { ::User.batch_user }
   let(:import_file) { described_class.new(record, data_path, user) }
@@ -27,13 +28,13 @@ describe Medspace::ImportFile do
     end
     describe '#file_path' do
       it 'returns the file path' do
-        expect(import_file.file_path.to_s).to eq(file_path)
+        expect(import_file.file_path(file).to_s).to eq(full_path)
       end
     end
 
     describe '#check_for_file' do
       it 'checks to see if a file exists at a path and returns true if it exists' do
-        expect(import_file.check_for_file(file_path)).to eq(true)
+        expect(import_file.check_for_file(import_file.file_path(file))).to eq(true)
       end
       it 'checks to see if a file exists at a path and returns false if it does not exist' do
         expect(import_file.check_for_file(bad_file_path)).to eq(false)
