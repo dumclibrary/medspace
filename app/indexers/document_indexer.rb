ea4815c112e915faee3a468a281ffd408b9bbdaa
Mark Bussey <mark@curationexperts.com>
2018-02-17 20:08:04 -0500

    Add a year index to Document work type
    Blacklight range limit requires an integer index, so we need to
    create an index of just the year portion of the sorting date as
    the corresponding 4-digit integer value.

--- app/indexers/document_indexer.rb
+++ app/indexers/document_indexer.rb
@@ -17,6 +17,37 @@ class DocumentIndexer < Hyrax::WorkIndexer
    super.tap do |solr_doc|
          titles = solr_doc[Solrizer.solr_name('title', :stored_searchable)] || []
          solr_doc['title_ssort'] = titles.to_sentence
+         solr_doc['year_iim'] = extract_years(object.date)
    end
   end
+
+  # Converts an array of strings into an array of corresponding years as integers
+  # @param dates [Array<String>] a list of dates in some textual format
+  # @return [Array<Integer>] a list of integers representing the years referenced in the dates
+  def extract_years(dates)
+    dates.flat_map{ |date| extract_year(date) }.uniq
+  end
+
+  # Return an integer corresponding to the year in a string representing a date
+  # Accepts years in form YYYY, iso8601, and ranges YYYY-YYYY
+  # @param date [String]
+  # @return [Array<Integer>] the four digit integer(s) corresponding to year(s) in the date or date range
+  def extract_year(date)
+    if date.blank?
+      [nil]
+    elsif /^\d{4}$/ =~ date
+      # Date.iso8601 doesn't support YYYY dates
+      [date.to_i]
+    elsif /^\d{4}-\d{4}$/ =~ date
+      # date range in YYYY-YYYY format
+      earliest, latest = date.split('-').flat_map{ |y| y.to_i }
+      (earliest..latest).to_a
+    else
+      [Date.iso8601(date).year]
+    end
+  rescue ArgumentError
+    raise "Invalid date: #{date.inspect}"
+  end
+
+
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -114,4 +114,50 @@ RSpec.describe Document do
     #expect(doc.resource.dump(:ttl)).to match(/www.loc.gov\/mods\/rdf\/v1#locationPhysical/)
   end
 
+  context 'a work with facetable years' do
+    
+    let(:work) { Document.new }
+
+    it 'indexes years in YYYY format' do
+      work.date = ["1835"]
+      expect(work.to_solr['year_iim']).to include 1835
+    end
+
+    it "indexes dates with multiple values" do
+      work.date = ["1441", "2018"]
+      expect(work.to_solr['year_iim']).to include 1441, 2018
+    end
+
+    it "indexes years in YYYY-MM-DD format" do
+      work.date = ["1973-04-09"]
+      expect(work.to_solr['year_iim']).to include 1973
+    end
+
+    it "indexes year ranges in the form YYYY-YYYY" do
+      work.date = ["1910-1919"]
+      expect(work.to_solr['year_iim']).to include 1910, 1915, 1919
+    end
+
+    it "indexes and deduplicates years" do
+      work.date = ["1847-1850","1848-1852"]
+      expect(work.to_solr['year_iim']).to contain_exactly 1847, 1848, 1849, 1850, 1851, 1852
+    end
+
+    it "indexes mixed date values" do
+      work.date = ["1847-1850", "2018-02-06", "1848-1852", "1850-01-15", "1441"]
+      expect(work.to_solr['year_iim']).to contain_exactly 1441, 1847, 1848, 1849, 1850, 1851, 1852, 2018
+    end
+
+    it 'returns nil when the date is empty' do
+      work.date = nil
+      expect(work.to_solr['year_iim']).to eq []
+    end
+
+    it 'raises an error on invalid dates' do
+      work.date = ["Mid 20th Century"]
+      expect { work.to_solr['year_iim'] }.to raise_error "Invalid date: #{work.date.first.inspect}"
+    end
+
+  end
+
 end
