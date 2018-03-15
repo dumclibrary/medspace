d9affda3c272cab09b04f4751fa1b9a88be6ddd1
Michael Ravenel <michael.ravenel@duke.edu>
2018-03-15 16:09:09 -0400

    Allow rails environment to be configured

--- ansible/roles/medspace/templates/medspace.sh
+++ ansible/roles/medspace/templates/medspace.sh
@@ -1,4 +1,4 @@
-export RAILS_ENV=production
+export RAILS_ENV={{ medspace_rails_env }}
 export RAILS_LOG_TO_STDOUT=true
 export FCREPO_URL=http://127.0.0.1:8080/fcrepo/rest
 export SOLR_URL=http://127.0.0.1:8983/solr/medspace
