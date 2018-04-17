d6cba635d381fb0623659538025ead86d70c9814
Russell Koonts <rskoonts@duke.edu>
2018-04-17 09:44:14 -0400

    updated Users passwords abilities

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   #devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
